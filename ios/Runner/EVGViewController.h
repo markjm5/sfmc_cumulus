9307c4ce88ebc7735c77ab5755f09a31b63e6ce9
Mark Mukherjee <65020036+markjm5@users.noreply.github.com>
2020-11-22 21:23:53 +0800

    iOS working: added new EVGViewController but needs to be hooked up

---
+++ ios/Runner/EVGViewController.h
@@ -0,0 +1,9 @@
+
+#import <UIKit/UIKit.h>
+#include <sys/cdefs.h>
+#import <Foundation/Foundation.h>
+#import <Flutter/Flutter.h>
+
+@interface EVGViewController:FlutterViewController
+
+@end
---
+++ ios/Runner/EVGViewController.m
@@ -0,0 +1,19 @@
+//
+//  EVGViewController.m
+//  Runner
+//
+//  Created by Mark Mukherjee on 21/11/20.
+//
+#import "EVGViewController.h"
+#import <Foundation/Foundation.h>
+#import <Flutter/Flutter.h>
+
+@implementation EVGViewController:FlutterViewController
+- (instancetype) init
+{
+    self = [super init];
+    return self;
+        
+}
+
+@end
