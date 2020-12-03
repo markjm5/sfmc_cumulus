01cf3e1e746e2d621e4f5b56609db771b0a263f9
Mark Mukherjee <65020036+markjm5@users.noreply.github.com>
2020-12-03 16:17:50 +0800

    iOS working: updates to comments

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -173,8 +173,8 @@ class _HomeState extends State<Home> {
                                     clipBehavior: Clip.antiAlias,
                                     child: InkWell(
                                       onTap: () {
-                                        //_registerTap('viewItem',i.productId, _interactionstudioLogEvent);
-                                        _registerTap('viewTag',"Fixed Income Securities|3", _interactionstudioLogEvent); //Play Around With This
+                                        //_registerTap('viewItem',i.productId, _interactionstudioLogEvent); // View Item
+                                        _registerTap('viewTag',"Fixed Income Securities|3", _interactionstudioLogEvent); //View Tag
 
                                         Navigator.pushNamed(
                                             context, '/products',
